ee94fd35baa09c1b0af8714944a4bd52031d0654
Gaetan Masson <gaetan@edyoucated.org>
2025-05-28 10:17:35 +0200

    Config: added middleware config for Vercel

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,11 +2,22 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:3000', // Development
+        'https://gaetan-masson-frontend.vercel.app/', // Production
+        'https://*.vercel.app', // Preview deployments
+      ]
+    }
+  },
 ];
